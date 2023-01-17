69daa40b6012d6e95d63e9341b536dbb5495d616
Software Developer <yinyinbei0717@gmail.com>
2023-01-16 20:58:19 -0600

    sdf

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -8,20 +8,20 @@ yum install gcc -y
 yum install build-essential -y
 yum install python3-pip python3-devel python3-setuptools -y
 
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.helper '!aws --profile default codecommit credential-helper $@'
-git config --system credential.https://git-codecommit.us-east-1.amazonaws.com.UseHttpPath true
+git config --system credential.https://git-codecommit.us-east-2.amazonaws.com.helper '!aws --profile default codecommit credential-helper $@'
+git config --system credential.https://git-codecommit.us-east-2.amazonaws.com.UseHttpPath true
 
-aws configure set region us-east-1
+aws configure set region us-east-2
 
 mkdir -p /var/www
 
-git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/flaskapp /var/www
+git clone https://git-codecommit.us-east-2.amazonaws.com/v1/repos/flasktodo /var/www
 
 cd /var/www
 
 git config core.fileMode false
 
-aws s3 cp s3://tci-s3-demo/flask-app/.env .env
+aws s3 cp s3://tci-s3-demo/flask-todo/.env .env
 
 chmod +x scripts/post_userdata.sh
 
